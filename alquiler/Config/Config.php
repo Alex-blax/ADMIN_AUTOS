4a78b1fcd152de84328c4a506e5ec378207ed1f0
Samuel Mercado <samuelmer18@hotmail.com>
2024-06-03 00:29:33 -0400

    conexion db en nube

--- alquiler/Config/Config.php
+++ alquiler/Config/Config.php
@@ -1,8 +1,7 @@
 <?php
 const base_url = "http://localhost/alquiler_vehiculos/alquiler/";
-const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "alquiler";
+const host = "sql10.freemysqlhosting.net";
+const user = "sql10711355";
+const pass = "WnbPi4pWsV";
+const db = "sql10711355";
 const charset = "charset=utf8";
-?>
